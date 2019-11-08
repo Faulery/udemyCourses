8954c23b58d48d66405469677a46721aa1675a0b
Yurii Ilnytskyi <yurii.ilnytskyi@gmail.com>
2019-11-08 08:32:18 +0200

    Databinding

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -9,10 +9,26 @@ export interface Element {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   serverElements: Element[] = [
-    {type: 'server', name: 'Test server', content: 'Just a test'}
+    { type: 'server', name: 'Test server', content: 'Just a test' }
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
