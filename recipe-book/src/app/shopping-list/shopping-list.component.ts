36e6b3aad256cbd6781cb8750b8a2082711fb59c
Yurii Ilnytskyi <yurii.ilnytskyi@gmail.com>
2019-11-18 20:59:45 +0200

    Renamed app

---
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit} from '@angular/core';
+import { Ingredient } from '../shared/ingredient.module';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
