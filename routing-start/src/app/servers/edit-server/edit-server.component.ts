420b295f0fd55c79f9e40b50955af685d8177047
Yurii_Ilnytskyi <yxi@cbord.com>
2019-12-11 17:31:52 +0200

    understanding router guards

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css'],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
+  canDeactivate (): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+    if (this.serverName !== this.server.name || this.serverStatus !== this.server.status && !this.changesSaved) {
+      return confirm('Do you wanna discard the changes?');
+    } else {
+      return true;
+    }
+  }
+
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = + this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
     this.route.queryParams.subscribe((queryParams: Params) => {
@@ -31,7 +47,9 @@ export class EditServerComponent implements OnInit {
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {
       name: this.serverName,
-      status: this.serverStatus
+      status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 }
