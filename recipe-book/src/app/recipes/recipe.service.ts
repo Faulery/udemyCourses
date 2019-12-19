92144af37603686b630ccd5c39c362a4ef7ea536
Yurii_Ilnytskyi <yxi@cbord.com>
2019-12-19 13:24:04 +0200

    Added route between components

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -28,6 +28,10 @@ export class RecipeService {
     return this.recipes.slice(); // To avoid any possible changes to original array from outside
   }
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   addIngredientToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
